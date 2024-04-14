0826f00bf1a943dc68adbe266598aad9d6719d50
LuckFire <39076891+LuckFire@users.noreply.github.com>
2024-04-14 18:55:52 -0400

    chore: removed unused constant

--- lib/util/constants.ts
+++ lib/util/constants.ts
@@ -1,5 +1,1 @@
 export const BASE_API_URL = new URL('https://apis.roblox.com');
-
-export const enum API_URLS {
-    GetEntry = '/datastores/v1/universes/{universeId}/standard-datastores/datastore/entries/entry'
-}
