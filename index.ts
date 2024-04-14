18962e3bdf53cc5de7da41d014f412ff7c0a560b
LuckFire <39076891+LuckFire@users.noreply.github.com>
2024-04-14 18:55:12 -0400

    chore: export types

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
 export * from '#lib/DataStoreService';
+
+export type * from '#lib/types/parameters';
+export type { OrderedDataStore } from '#lib/structures/OrderedDataStores/OrderedDataStore';
+export type { OrderedDataStoreEntry } from '#lib/structures/OrderedDataStores/OrderedDataStoreEntry';
